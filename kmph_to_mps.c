217f575899ff36adf6ae5f54920f74324da985eb
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2023-10-19 18:47:53 +0530

    #include<stdio.h> int main() {     int km;     scanf("%d",&km);     float m=(5*km)/18.0;     printf("%0.2f",m); }

--- kmph_to_mps.c
+++ kmph_to_mps.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int kilometers;
-    scanf("%d",&kilometers);
-    float meters=(5*kilometers)/18.0;
-    printf("%0.2f",meters);
+    int km;
+    scanf("%d",&km);
+    float m=(5*km)/18.0;
+    printf("%0.2f",m);
 }
