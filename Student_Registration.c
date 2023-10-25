23aa00cfec94da4e0e853f09e5a5b7279601f391
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2023-10-25 13:17:38 +0530

    #include<stdio.h> int main() {     int n,m,k,a,b,c,t;     scanf("%d",&t);     scanf("%d %d %d",&n,&m,&k);
        scanf("%d %d %d",&a,&b,&c);
      int capacity=m-k;
        int cap=b-c;
        int result=capacity-n;
      int  res=cap-a;
    {
        if(result>0) printf("YES
    ");
        else    printf("NO
    ");
        
    }
        
        { 
            if(res>0) printf("YES");
        else    printf("NO");
        }
    }

---
+++ Student_Registration.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,k,a,b,c,t;
+    scanf("%d",&t);
+    scanf("%d %d %d",&n,&m,&k);
+    
+    scanf("%d %d %d",&a,&b,&c);
+  int capacity=m-k;
+    int cap=b-c;
+    int result=capacity-n;
+  int  res=cap-a;
+{
+    if(result>0) printf("YES
+");
+    else    printf("NO
+");
+    
+}
+    
+    { 
+        if(res>0) printf("YES");
+    else    printf("NO");
+    }
+}
