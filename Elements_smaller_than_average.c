2023035189e4b2bdbdb672f24f45a4c958fde8a4
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2023-12-19 11:48:47 +0530

    #include<stdio.h> int main() {     int n,s=0.0,c=0;     scanf("%d",&n);     int arr[n],i;     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);         s=s+arr[i];     }     float  r=s/n;     for(i=0; i<n; i++)     {         if(arr[i]<=r)  c++;     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,s=0.0,c=0;
+    scanf("%d",&n);
+    int arr[n],i;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+        s=s+arr[i];
+    }
+    float  r=s/n;
+    for(i=0; i<n; i++)
+    {
+        if(arr[i]<=r)  c++;
+    }
+    printf("%d",c);
+}
