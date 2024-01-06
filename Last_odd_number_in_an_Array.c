8f2dc2236ac5c4231880cd5472d2205b4aae3aa1
gollapallijayanthi <148422162+gollapallijayanthi@users.noreply.github.com>
2024-01-06 22:51:47 +0530

    #include<stdio.h> int main() {     int n,i,s=0,flag=0;     scanf("%d",&n);     int arr[n];     for(i=0; i<n; i++)     {         scanf("%d",&arr[i]);         s=s+arr[i];     }     int r;     scanf("%d",&r);     for(i=n-1; i>=0; i--)     {         if(arr[i]%2!=0)         {             printf("%d",arr[i]);             break;
            }
        }
       
        
    }

--- Last_odd_number_in_an_Array.c
+++ Last_odd_number_in_an_Array.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,i,s=0,flag=0;
     scanf("%d",&n);
-    int arr[n],i;
+    int arr[n];
     for(i=0; i<n; i++)
     {
         scanf("%d",&arr[i]);
+        s=s+arr[i];
     }
+    int r;
+    scanf("%d",&r);
     for(i=n-1; i>=0; i--)
     {
         if(arr[i]%2!=0) 
         {
-            printf("%d ",arr[i]);
+            printf("%d",arr[i]);
             break;
+            
         }
     }
+   
+    
 }
